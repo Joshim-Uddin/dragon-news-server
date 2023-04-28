575fa6ed28899f118f2be69f3291d5d98cd7e11f
Joshim Uddin <mju881@gmail.com>
2023-04-28 11:10:28 +0600

    server ready

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express()
+const port = 5000;
+const cors = require('cors')
+
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
+
+app.use(cors())
+app.get('/', (req, res)=>{
+    res.send('Server is running')
+})
+
+app.get('/categories', (req, res)=>{
+    res.send(categories)
+})
+app.get('/categories/:id', (req, res)=>{
+    const id = req.params.id;
+    if(id == 0){
+        res.send(news)
+    }else{
+        const categorisedNews = news.filter(n=>n.category_id ===id)
+        res.send(categorisedNews)
+    }
+    
+})
+app.get('/news', (req, res)=>{
+    res.send(news)
+})
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id;
+    console.log(id)
+    const selectedNews = news.find(n=>n._id === id)
+    res.send(selectedNews)
+
+})
+app.listen(port,()=>{
+    console.log(`Local Server is running ${port}`)
+})
